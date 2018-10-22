b76dc169930d43508d56270365492aa1becc7703
Jessica Liu <jessicaliu006@gmail.com>
2018-10-22 12:34:27 -0400

    initial commit for extension

---
+++ server/src/main/extension/content_scripts/calendue.js
@@ -0,0 +1,26 @@
+/** Based on code from MDN web docs tutorial
+ * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_second_WebExtension
+ */
+
+(function() {
+    /**
+     * Check and set a global guard variable.
+     * If this content script is injected into the same page again,
+     * it will do nothing next time.
+     */
+    if (window.hasRun) {
+        return;
+    }
+    window.hasRun = true;
+
+    /**
+     * Listen for messages from the background script.
+     * Call "beastify()" or "reset()".
+     */
+    browser.runtime.onMessage.addListener((message) => {
+        if (message.command === "add-course") {
+            dump(message.pageContent);
+        }
+    });
+
+})();
