17b805cdcfcb2a8fa69962861827b76ad6f389ee
Jessica Liu <jessicaliu006@gmail.com>
2018-11-07 14:31:42 -0500

    Create form to sign up for account in extension

---
+++ server/src/main/extension/content_scripts/calendue.js
@@ -0,0 +1,55 @@
+/** Based on code from MDN web docs tutorial
+ * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_second_WebExtension
+ */
+
+(function() {
+    // /**
+    //  * Check and set a global guard variable.
+    //  * If this content script is injected into the same page again,
+    //  * it will do nothing next time.
+    //  */
+    // if (window.hasRun) {
+    //     return;
+    // }
+    // window.hasRun = true;
+
+    /**
+     * Sends the account data to the server to create the account using a POST request.
+     * @param username  the provided username of the user
+     * @param password  the provided password of the user
+     * @param email     the provided email of the user
+     */
+    function createAccount(username, password, email) {
+        var data = new FormData();
+        data.append("username", username);
+        data.append("password", password);
+        data.append("email", email);
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                console.log(this.responseText);
+            }
+        };
+
+        xhr.open("POST", "http://localhost:7000/accounts");
+        xhr.send(data);
+    }
+
+
+    /**
+     * Listen for messages from the background script.
+     */
+    browser.runtime.onMessage.addListener((message) => {
+        console.log(message.command);
+        if (message.command === "add-course") {
+            var content = document.documentElement.innerHTML;
+            console.log(content);
+        } else if (message.command === "create-account") {
+            createAccount(message.username, message.password, message.email);
+            console.log(message.username+" "+message.password+" "+message.email);
+        }
+    });
+
+})();
