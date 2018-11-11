be09bedf57244ecb3b78ad6446f1df906084a610
Idean <ideanl@users.noreply.github.com>
2018-11-11 16:30:02 -0500

    Revert "Updated main webpage with Header and search bar"

--- server/src/main/java/com/github/jhu_oose11/calendue/controllers/CalendarController.java
+++ server/src/main/java/com/github/jhu_oose11/calendue/controllers/CalendarController.java
@@ -1,12 +1,11 @@
 package com.github.jhu_oose11.calendue.controllers;
 
-import com.github.jhu_oose11.calendue.TemplatePath;
-import com.github.jhu_oose11.calendue.controllers.Helpers.Render;
 import io.javalin.Context;
 
 public class CalendarController {
     public static void index(Context ctx) {
         LoginController.ensureLoggedIn(ctx);
-        Render.render(ctx, TemplatePath.CALENDAR);
+
+        ctx.html("You are logged in. <a href='/logout'>Logout</a>");
     }
 }
