171cdad1ef2e8d124546e47d98d0e706a432f9bd
dchang38 <dchang38@jhu.edu>
2018-11-11 18:52:42 -0500

    Updated main webpage with Header and search bar

--- server/src/main/java/com/github/jhu_oose11/calendue/controllers/CalendarController.java
+++ server/src/main/java/com/github/jhu_oose11/calendue/controllers/CalendarController.java
@@ -1,11 +1,12 @@
 package com.github.jhu_oose11.calendue.controllers;
 
+import com.github.jhu_oose11.calendue.TemplatePath;
+import com.github.jhu_oose11.calendue.controllers.Helpers.Render;
 import io.javalin.Context;
 
 public class CalendarController {
     public static void index(Context ctx) {
         LoginController.ensureLoggedIn(ctx);
-
-        ctx.html("You are logged in. <a href='/logout'>Logout</a>");
+        Render.render(ctx, TemplatePath.CALENDAR);
     }
 }
