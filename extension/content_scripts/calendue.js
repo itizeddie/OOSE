fc7e0950e39080d39048b62e03cf389b17fd1305
Jessica Liu <jessicaliu006@gmail.com>
2018-11-11 21:14:56 -0500

    Send DOM string from extension to server

--- extension/content_scripts/calendue.js
+++ extension/content_scripts/calendue.js
@@ -37,6 +37,39 @@
         xhr.send(data);
     }
 
+    function sendDOMtoServer(token, url, document) {
+        var data = new FormData();
+        data.append("token", token);
+        data.append("url", url);
+        data.append("document", document);
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                console.log(this.responseText);
+            }
+        };
+
+        xhr.open("POST", "http://localhost:7000/scrape");
+        xhr.send(data);
+    }
+
+    /**
+     * Creates UUID. Function taken from
+     * https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
+     * @returns {string}
+     */
+    function create_UUID(){
+        var dt = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (dt + Math.random()*16)%16 | 0;
+            dt = Math.floor(dt/16);
+            return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+    }
 
     /**
      * Listen for messages from the background script.
@@ -45,7 +78,10 @@
         console.log(message.command);
         if (message.command === "add-course") {
             var content = document.documentElement.innerHTML;
-            console.log(content);
+            var url = window.location.href;
+            var token = create_UUID();
+            sendDOMtoServer(token, url, content);
+            console.log(url+" "+token+" "+content);
         } else if (message.command === "create-account") {
             createAccount(message.username, message.password, message.email);
             console.log(message.username+" "+message.password+" "+message.email);
